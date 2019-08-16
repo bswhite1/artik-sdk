37f7f0476f215f813386c5b22edd3295dbeb93bd
Ben White <bwhite@swinetechnologies.com>
2019-08-16 15:39:23 +0000

    tweaking build

--- builddeb.sh
+++ builddeb.sh
@@ -5,15 +5,15 @@ if [ "$#" -ne 1 ]; then
 	exit
 fi
 TARGET_ARCH=$1
-BUILD_DIR=build-$TARGET_ARCH
+BUILD_DIR=.
 echo "Tager Architecture is $TARGET_ARCH"
 echo "When cross-compiling, do not forget to set the environment variables CROSS_COMPILE, SYSROOT and CMAKE_PKG_CONFIG_PATH"
-rm -rf $BUILD_DIR/
-mkdir -p $BUILD_DIR/package/src/
-git archive -o update.tgz HEAD
-mv update.tgz $BUILD_DIR/package/src/
-cd $BUILD_DIR/package/src/
-tar -xvf update.tgz
-rm -f update.tgz
+#rm -rf $BUILD_DIR/
+#mkdir -p $BUILD_DIR/package/src/
+#git archive -o update.tgz HEAD
+#mv update.tgz $BUILD_DIR/package/src/
+#cd $BUILD_DIR/package/src/
+#tar -xvf update.tgz
+#rm -f update.tgz
 JOBS=$(nproc)
 DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -j$JOBS -d -uc -us -a $TARGET_ARCH --target-arch $TARGET_ARCH
